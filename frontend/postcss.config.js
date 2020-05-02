a770b48441d7f17d0f9c5154e4cf5dbc00f3a9d9
Julian Tölle <julian.toelle97@gmail.com>
2020-05-02 18:41:57 +0200

    chore(frontend): update deps

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -3,13 +3,13 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
   content: ["./public/**/*.html", "./src/**/*.tsx"],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
 });
 
 module.exports = {
   plugins: [
     require("tailwindcss"),
     require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
-  ]
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
 };
