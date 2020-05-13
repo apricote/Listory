367f37555fa5ccfbadf7f690137eed22ff82a0bd
Julian Tölle <julian.toelle97@gmail.com>
2020-05-13 21:22:56 +0200

    fix(frontend): redirect to home after login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { LoginFailure } from "./components/LoginFailure";
+import { LoginSuccess } from "./components/LoginSuccess";
 import { NavBar } from "./components/NavBar";
 import { RecentListens } from "./components/RecentListens";
 import { ReportListens } from "./components/ReportListens";
@@ -21,6 +22,7 @@ export function App() {
       </header>
       <Switch>
         <Route path="/" exact />
+        <Route path="/login/success" exact component={LoginSuccess} />
         <Route path="/login/failure" exact component={LoginFailure} />
         <Route path="/listens" exact component={RecentListens} />
         <Route path="/reports/listens" exact component={ReportListens} />
---
+++ frontend/src/components/LoginSuccess.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+
+export const LoginSuccess: React.FC = () => {
+  return <Redirect to="/" />;
+};
