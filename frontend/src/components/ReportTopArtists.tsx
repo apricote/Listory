1ae7f08dc430d6fa9088ea19d7c7fb3e89c69000
Julian Tölle <julian.toelle97@gmail.com>
2020-11-07 19:23:57 +0100

    fix(frontend): missing key property in list

--- frontend/src/components/ReportTopArtists.tsx
+++ frontend/src/components/ReportTopArtists.tsx
@@ -57,7 +57,7 @@ export const ReportTopArtists: React.FC = () => {
           )}
           {reportHasItems &&
             report.map(({ artist, count }) => (
-              <div>
+              <div key={artist.id}>
                 {count} - {artist.name}
               </div>
             ))}
