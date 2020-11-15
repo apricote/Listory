09858076bf26367f52435b54ab0c5a992178104b
Julian Tölle <julian.toelle97@gmail.com>
2020-11-15 02:37:01 +0100

    refactor(api): optimize imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,15 @@ import { ScheduleModule } from "@nestjs/schedule";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
 import { AuthModule } from "./auth/auth.module";
+import { ConfigModule } from "./config/config.module";
 import { DatabaseModule } from "./database/database.module";
+import { HealthCheckModule } from "./health-check/health-check.module";
 import { ListensModule } from "./listens/listens.module";
 import { LoggerModule } from "./logger/logger.module";
 import { MusicLibraryModule } from "./music-library/music-library.module";
+import { ReportsModule } from "./reports/reports.module";
 import { SourcesModule } from "./sources/sources.module";
 import { UsersModule } from "./users/users.module";
-import { ConfigModule } from "./config/config.module";
-import { HealthCheckModule } from "./health-check/health-check.module";
-import { ReportsModule } from "./reports/reports.module";
 
 @Module({
   imports: [
