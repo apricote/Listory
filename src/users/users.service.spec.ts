ed1e7f8e461d94d5c70ffad8a7ace1b0b5ad67c0
Julian Tölle <julian.toelle97@gmail.com>
2023-03-12 02:13:59 +0100

    feat(api): import listens after first sign in
    Previously it took a minute until the import was started within the normal
    loop. This way, we create an import job right after the user logs
    in for the first time.

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,25 +1,36 @@
+import { JobService } from "@apricote/nest-pg-boss";
 import { Test, TestingModule } from "@nestjs/testing";
+import { IImportSpotifyJob, ImportSpotifyJob } from "../sources/jobs";
 import { UserRepository } from "./user.repository";
 import { UsersService } from "./users.service";
 
 describe("UsersService", () => {
   let service: UsersService;
   let userRepository: UserRepository;
+  let importSpotifyJobService: JobService<IImportSpotifyJob>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
         { provide: UserRepository, useFactory: () => ({}) },
+        {
+          provide: ImportSpotifyJob.ServiceProvider.provide,
+          useFactory: () => ({}),
+        },
       ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
     userRepository = module.get<UserRepository>(UserRepository);
+    importSpotifyJobService = module.get<JobService<IImportSpotifyJob>>(
+      ImportSpotifyJob.ServiceProvider.provide
+    );
   });
 
   it("should be defined", () => {
     expect(service).toBeDefined();
     expect(userRepository).toBeDefined();
+    expect(importSpotifyJobService).toBeDefined();
   });
 });
