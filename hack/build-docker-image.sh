86d58370d3112d6812dc3c367e9d6a8e2cf06dae
Julian Tölle <julian.toelle97@gmail.com>
2023-04-02 16:05:29 +0200

    ci: optimize speed by avoiding a busted cache after prepare step

--- hack/build-docker-image.sh
+++ hack/build-docker-image.sh
@@ -11,17 +11,14 @@ REPO="apricote/listory"
 PLATFORMS="--platform=linux/amd64,linux/arm64"
 TAGS="--tag ${REPO}:${VERSION} --tag ${REPO}:latest"
 ARGS="--build-arg VERSION=${VERSION} --build-arg GIT_COMMIT=`git rev-parse HEAD`"
+CACHE="--cache-from=type=registry,ref=${REPO}:buildcache --cache-to=type=registry,ref=${REPO}:buildcache"
 
-CACHE=""
+
+# We "build" the image twice, once in "prepare" and then again in "publish" stage:
+# - Prepare makes sure that the image is buildable
+# - Publish utilizes the local cache from prepare stage and pushes the image
 PUSH=""
-
-# We "build" the image twice, once in "prepare" and then again in "publish" stage.
-# - Prepare makes sure that the image is buildable and utilizes the remote cache.
-# - Publish utilizes the local cache from prepare stage and pushes the image.
-if [ "$PREPARE_OR_PUBLISH" = "prepare" ]; then
-  CACHE="--cache-from=type=registry,ref=${REPO}:buildcache --cache-to=type=registry,ref=${REPO}:buildcache"
-else
-  # Uses local buildkit cache
+if [ "$PREPARE_OR_PUBLISH" = "publish" ]; then
   PUSH="--push"
 fi
 
