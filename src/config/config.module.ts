bbc7ec08174c070f8eda16a6bf3357e8a5262e70
Julian Tölle <julian.toelle97@gmail.com>
2023-05-07 13:36:55 +0200

    fix: missing import for @hapi/joi (#274)
    Replaced the module in the dependencies but not in actual usage

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,6 +1,6 @@
-import * as Joi from "@hapi/joi";
 import { Module } from "@nestjs/common";
 import { ConfigModule as NestConfigModule } from "@nestjs/config";
+import * as Joi from "joi";
 
 @Module({
   imports: [
